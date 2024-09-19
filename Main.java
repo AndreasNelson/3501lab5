bab994e80c953194dcdac9ab99a9fc8f576a2283
Andreas Nelson <nel02254@morris.umn.edu>
2024-09-19 13:12:16 -0500

    created files and started ranking array foundations

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Andreas Nelson
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Ranking.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Ranking {
+    private ArrayList<String> Programmers;
+    private ArrayList<Integer> Companies; 
+
+    public Ranking() {
+        this.Programmers = new ArrayList<String>();
+        this.Companies = new ArrayList<Integer>();
+    }
+}
